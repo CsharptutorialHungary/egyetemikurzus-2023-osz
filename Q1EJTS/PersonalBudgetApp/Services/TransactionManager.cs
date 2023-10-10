86187efa8b9682ddb411ce3d09cea10659ff51da
kvcspt <kvcspt0@gmail.com>
2023-10-10 09:57:52 +0200

    Mappastruktúra felállítása, TransactionManager osztály hozzáadása

---
+++ Q1EJTS/PersonalBudgetApp/Services/TransactionManager.cs
@@ -0,0 +1,19 @@
+﻿using PersonalBudgetApp.Transactions;
+
+namespace PersonalBudgetApp.Services
+{
+    internal class TransactionManager
+    {
+        private static IEnumerable<Transaction> Transactions = new List<Transaction>();
+
+        public IEnumerable<Transaction> Transaction
+        {
+            get { return Transactions; }
+        }
+        public void ClearTransactions()
+        {
+            Transactions = new List<Transaction>();
+        }
+
+    }
+}
