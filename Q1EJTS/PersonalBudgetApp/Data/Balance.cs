4021e4b950f33fc59a8fc1ab6cbd00485417fd70
kvcspt <kvcspt0@gmail.com>
2023-10-23 16:40:31 +0200

    Egyenleg osztály megvalósítása

---
+++ Q1EJTS/PersonalBudgetApp/Data/Balance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBudgetApp.Data
+{
+    class Balance
+    {
+        private Money _currentBalance;
+        private static Balance? instance;
+        public static Balance Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Balance();
+                }
+                return instance;
+            }
+        }
+        private Balance()
+        {
+            _currentBalance = new Money(0);
+            
+        }
+
+        public void AddMoneyToCurrentBalance(Money money)
+        {
+            this._currentBalance = new Money(this._currentBalance.Amount + money.Amount);
+        }
+        public void SubtractMoneyFromCurrentBalance(Money money)
+        {
+            this._currentBalance = new Money(this._currentBalance.Amount - money.Amount);
+        }
+
+    }
+}
