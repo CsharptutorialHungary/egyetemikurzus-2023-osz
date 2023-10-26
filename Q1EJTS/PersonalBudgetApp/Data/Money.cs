78e08c15b8b6f23dd5cfaa358c73cee8082a2713
kvcspt <kvcspt0@gmail.com>
2023-10-26 16:53:50 +0200

    Operátorok felülírása a Money osztályban

--- Q1EJTS/PersonalBudgetApp/Data/Money.cs
+++ Q1EJTS/PersonalBudgetApp/Data/Money.cs
@@ -1,11 +1,40 @@
-﻿namespace PersonalBudgetApp.Data
+﻿using System.Numerics;
+
+namespace Q1EJTS.PersonalBudgetApp.Data
 {
-    public record Money(decimal Amount, string Currency)
+    public record Money(decimal Amount)
     {
         public override string? ToString()
         {
-            return $"{Amount} {Currency}";
+            return $"{Amount} HUF";
         }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static implicit operator decimal(Money money)
+        {
+            return money.Amount;
+        }
+
+        public static Money operator -(Money money1, Money money2)
+        {
+            decimal resultAmount = money1.Amount - money2.Amount;
+            return new Money(resultAmount);
+        }
+
+        public static Money operator +(Money money1, Money money2)
+        {
+            decimal resultAmount = money1.Amount + money2.Amount;
+            return new Money(resultAmount);
+        }
+        public static Money operator -(Money money1)
+        {
+            return new Money(-money1.Amount);
+        }
+
     }
 
 }
