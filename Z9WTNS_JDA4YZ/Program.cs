719547d5a5bed22fee0a0d0d1a9f913955f4e5d2
h150714 <h150714@stud.u-szeged.hu>
2023-11-04 16:26:10 +0100

    changes

--- Z9WTNS_JDA4YZ/Program.cs
+++ Z9WTNS_JDA4YZ/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using Z9WTNS_JDA4YZ.CLI;
+﻿using Z9WTNS_JDA4YZ.CLI;
 using Z9WTNS_JDA4YZ.CLI.Commands;
 using Z9WTNS_JDA4YZ.DataClasses;
 using Z9WTNS_JDA4YZ.XML;
@@ -10,14 +9,6 @@ namespace Z9WTNS_JDA4YZ
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            
-=======
-            string PROGRAM_PATH = Assembly.GetAssembly(typeof(Program))?.Location ?? ".\\";
-            const string USERS_PATH = @".\data\users.xml";
-            const string TRANSACTIONS_PATH = @".\data\transactions.xml";
->>>>>>> c9e79a340a6d80b0b7cba6ae52a4c06f4b680ff9
-
             if (!XMLHandler.InitializeXMLData(PathConst.USERS_PATH) || !XMLHandler.InitializeXMLData(PathConst.TRANSACTIONS_PATH))
             {
                 Console.Write(PathConst.USERS_PATH, "\n");
@@ -25,21 +16,15 @@ namespace Z9WTNS_JDA4YZ
                 return;
             }
 
-            /*CommandRunner commandRunner = new CommandRunner
+            CommandRunner commandRunner = new CommandRunner
             {
                 Message = "\rChoose an option ('login' or 'register' or 'exit'): ",
                 Commands = new ICommand[] {new ExitCommand(), new LoginCommand(), new RegisterCommand()}
             };
             
-            commandRunner.Run();
-             */
+            User user = (User)commandRunner.Run();
 
-            List<User> users = new List<User>()
-            {
-                new User(0, "Jenő", "asdasdasdasdasd")
-            };
-
-            XMLHandler.writeObjectsToXML(PathConst.USERS_PATH, users);
+            Console.WriteLine(user);
         }
     }
 }
--- Z9WTNS_JDA4YZ/XML/PathConst.cs
+++ Z9WTNS_JDA4YZ/XML/PathConst.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 
 namespace Z9WTNS_JDA4YZ.XML
 {
     internal static class PathConst
     {
+        public static string USERS_PATH { get => Path.Combine(new string[] {GetProgramPath(), "data", "users.xml" }); }
 
+        public static string TRANSACTIONS_PATH { get => Path.Combine(new string[] { GetProgramPath(), "data", "transactions.xml" }); }
 
-        public static string USERS_PATH = Path.Combine(new string[] { Path.GetDirectoryName(Assembly.GetAssembly(typeof(PathConst))?.Location) , "users.xml" });//".\\", "data","users.xml "});
-        public static string TRANSACTIONS_PATH = Path.Combine(new string[] { Assembly.GetAssembly(typeof(Program))?.Location ?? ".\\", "data", "transactions.xml " });
-
+        private static string GetProgramPath()
+        {
+            return Path.GetDirectoryName(Assembly.GetAssembly(typeof(PathConst))?.Location)!;
+        }
     }
 }
