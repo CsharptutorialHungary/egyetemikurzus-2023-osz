2a8850e4f67f3c89407f6ea842c3855494f03c6d
Rudnay Roland <h163369@stud.u-szeged.hu>
2023-11-05 15:36:36 +0100

    hashed password

--- Z9WTNS_JDA4YZ/AccountHandler.cs
+++ Z9WTNS_JDA4YZ/AccountHandler.cs
@@ -1,5 +1,9 @@
-﻿using Z9WTNS_JDA4YZ.DataClasses;
+﻿using System;
+using System.Collections.Generic;
+using Z9WTNS_JDA4YZ.DataClasses;
 using Z9WTNS_JDA4YZ.Xml;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Z9WTNS_JDA4YZ
 {
@@ -8,23 +12,23 @@ namespace Z9WTNS_JDA4YZ
         internal static User? Login()
         {
             List<User> users = XmlHandler.ReadObjectsFromXml<User>(PathConst.USERS_PATH);
-            Console.WriteLine("Bejelentkezés");
+            Console.WriteLine("Kérem jelentkezen be!");
             Console.WriteLine("---------------------------------------");
             Console.WriteLine("Kérem, írja be a Nevét: ");
             string name = Console.ReadLine();
             Console.WriteLine("Kérem, írja be a jelszavát ");
             string password = Console.ReadLine();
+
             foreach (var user in users)
             {
-                if(user.Username.Equals(name) || user.HashedPassword.Equals(password))
+                if (user.Username.Equals(name) && VerifyPassword(password, user.HashedPassword))
                 {
                     Console.WriteLine("Sikeresen bent vagy");
-                    break;
+                    return user;
                 }
-                else { Console.WriteLine("Valami nem jó"); break; }
-
             }
 
+            Console.WriteLine("Belépés sikertelen");
             return null;
         }
 
@@ -36,25 +40,44 @@ namespace Z9WTNS_JDA4YZ
             string name = Console.ReadLine();
             Console.WriteLine("Kérem, írja be a jelszavát ");
             string password = Console.ReadLine();
-            if(XmlHandler.AppendObjectsToXml<User>(PathConst.USERS_PATH, new List<User>() { new User(users.Count + 1, name, password) }))
-            {Console.WriteLine("Sikeres volt a regisztráció");
-                Login();
+            string hashedPassword = HashPassword(password);
+
+            if (XmlHandler.AppendObjectsToXml<User>(PathConst.USERS_PATH, new List<User> { new User(users.Count + 1, name, hashedPassword) }))
+            {
+                Console.WriteLine("Sikeres volt a regisztráció");
+                return Login();
             }
-            else { Console.WriteLine("Valami nem jó"); }
-            
+            else
+            {
+                Console.WriteLine("Regisztráció sikertelen");
+            }
 
             return null;
         }
 
         internal static void AddTransaction(User user)
         {
-
+            // Itt adhatsz hozzá tranzakciókat a felhasználóhoz
         }
 
         internal static void QueryStatistics(User user)
         {
-
+            // Itt lekérdezheted a felhasználó statisztikáit
         }
 
+        private static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+            }
+        }
+
+        private static bool VerifyPassword(string password, string hashedPassword)
+        {
+            string newPasswordHash = HashPassword(password);
+            return newPasswordHash == hashedPassword;
+        }
     }
 }
