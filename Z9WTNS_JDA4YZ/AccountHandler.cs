4eddc19ba6c0c0789a899f9d575301b39a0d2559
Rudnay Roland <h163369@stud.u-szeged.hu>
2023-11-05 15:19:38 +0100

    reg

--- Z9WTNS_JDA4YZ/AccountHandler.cs
+++ Z9WTNS_JDA4YZ/AccountHandler.cs
@@ -8,20 +8,25 @@ namespace Z9WTNS_JDA4YZ
         internal static User? Login()
         {
             List<User> users = XmlHandler.ReadObjectsFromXml<User>(PathConst.USERS_PATH);
-            
+            Console.WriteLine("Bejelentkezés");
 
             return null;
         }
 
         internal static User? Register()
         {
+            Console.WriteLine("Most a regisztrációs menü sorban vagy!");
             List<User> users = XmlHandler.ReadObjectsFromXml<User>(PathConst.USERS_PATH);
+            Console.WriteLine("Kérem, írja be a Nevét: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Kérem, írja be a jelszavát ");
+            string password = Console.ReadLine();
+            if(XmlHandler.AppendObjectsToXml<User>(PathConst.USERS_PATH, new List<User>() { new User(users.Count + 1, name, password) }))
+            {Console.WriteLine("Sikeres volt a regisztráció");
+                Login();
+            }
+            else { Console.WriteLine("Valami nem jó"); }
             
-            XmlHandler.AppendObjectsToXml<User>(PathConst.USERS_PATH, new List<User>() { new User(users.Count+1, "jeno", "123") });
-
-            Console.WriteLine("regiszteáricó");
-
-
 
             return null;
         }
@@ -35,5 +40,6 @@ namespace Z9WTNS_JDA4YZ
         {
 
         }
+
     }
 }
