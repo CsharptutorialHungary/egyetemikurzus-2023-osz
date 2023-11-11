4aba49e52df6d5d6e1c99868a15faa803cf02a36
kvcspt <kvcspt0@gmail.com>
2023-11-11 17:22:52 +0100

    Interfészek bevezetése

---
+++ Q1EJTS/PersonalBudgetApp/Services/IBalanceManager.cs
@@ -0,0 +1,17 @@
+﻿using Q1EJTS.PersonalBudgetApp.Data;
+using Q1EJTS.PersonalBudgetApp.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1EJTS.PersonalBudgetApp.Services
+{
+    internal interface IBalanceManager
+    {
+        public Money GetCurrentBalance();
+        void UpdateBalance(Transaction transaction);
+        
+    }
+}
---
+++ Q1EJTS/PersonalBudgetApp/UserInterface/IUserInput.cs
@@ -0,0 +1,15 @@
+﻿using Q1EJTS.PersonalBudgetApp.Categories;
+using Q1EJTS.PersonalBudgetApp.Data;
+using Q1EJTS.PersonalBudgetApp.Query;
+
+namespace Q1EJTS.PersonalBudgetApp.UserInterface
+{
+    interface IUserInput
+    {
+        string GetFilePathFromUserInput();
+        DateTime GetValidDateFromUserInput();
+        Money GetMoneyAmountFromUserInput();
+        FinancialCategory GetFinancialCategoryFromUserInput();
+        SortingOrder GetSortingOrderFromUserInput();
+    }
+}
