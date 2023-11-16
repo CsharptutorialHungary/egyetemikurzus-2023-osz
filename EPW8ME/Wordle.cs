756a338b8c69880f00a53cad37b76ad32ef5185b
Mate Bence <h267789@stud.u-szeged.hu>
2023-11-16 23:45:42 +0100

    project created

---
+++ EPW8ME/Wordle.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace EWP8ME
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
