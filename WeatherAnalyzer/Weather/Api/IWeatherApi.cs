ed59913f521325d11359379fec032cfc6f1ebb67
opekope2 <opekope2@gmail.com>
2023-11-17 23:08:14 +0100

    Refactor IWeatherApi
    Rename to IWeatherForecastApi
    Make GetWeatherForecasts async

--- WeatherAnalyzer/Weather/Api/IWeatherApi.cs
+++
@@ -1,8 +0,0 @@
-using WeatherAnalyzer.Geocode;
-
-namespace WeatherAnalyzer.Weather.Api;
-
-public interface IWeatherApi
-{
-    IEnumerable<WeatherForecast> GetWeatherForecasts(Location location);
-}
