7edafb11353bb6e425d8acc96164db061845f3fb
opekope2 <opekope2@gmail.com>
2023-11-19 22:37:02 +0100

    Change wind direction to int
    As per OpenMeteo API response

--- WeatherAnalyzer/Weather/Wind.cs
+++ WeatherAnalyzer/Weather/Wind.cs
@@ -2,4 +2,4 @@ using WeatherAnalyzer.Util;
 
 namespace WeatherAnalyzer.Weather;
 
-public record struct Wind(ValueUnit<float> Speed, ValueUnit<float> Direction);
+public record struct Wind(ValueUnit<float> Speed, ValueUnit<int> Direction);
