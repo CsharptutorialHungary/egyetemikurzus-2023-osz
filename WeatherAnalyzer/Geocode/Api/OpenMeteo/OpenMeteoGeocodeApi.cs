a8aa7f2f44ad9f815bee9df34ec7f8999506d6f7
opekope2 <opekope2@gmail.com>
2023-11-20 23:51:28 +0100

    Add OpenMeteo geocode API

---
+++ WeatherAnalyzer/Geocode/Api/OpenMeteo/OpenMeteoGeocodeApi.cs
@@ -0,0 +1,77 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Configuration;
+using WeatherAnalyzer.Cli;
+
+namespace WeatherAnalyzer.Geocode.Api.OpenMeteo;
+
+public class OpenMeteoGeocodeApi(string apiUrl, string languageCode) : IGeocodeApi, ICliDiscoverableApi
+{
+    private const string DefaultLanguage = "en";
+    private const string DefaultApiUrl = "https://geocoding-api.open-meteo.com/v1/search";
+    private const int MaxResultCount = 10;
+
+    [SuppressMessage("ReSharper", "UnusedMember.Global")] // Used by reflection
+    public OpenMeteoGeocodeApi(IConfiguration configuration) : this(
+        configuration["OpenMeteo:GeocodeApiUrl"] ?? DefaultApiUrl,
+        configuration["Language"] ?? DefaultLanguage
+    )
+    {
+    }
+
+    public string CliName => "open-meteo";
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [return: NotNull]
+    private static T NullCheck<T>(T value, [CallerArgumentExpression(nameof(value))] string expr = null!)
+    {
+        if (value is null)
+        {
+            throw new InvalidOperationException($"API error: '{expr}' was null");
+        }
+
+        return value;
+    }
+
+    public async IAsyncEnumerable<City> FindCitiesAsync(string name)
+    {
+        using var http = new HttpClient();
+
+        var stream = await http.GetStreamAsync(
+            $"{apiUrl}?name={name}&count={MaxResultCount}&language={languageCode}&format=json");
+
+        var response = await JsonSerializer.DeserializeAsync<ApiResponse>(stream);
+        if (response is null) yield break;
+
+        foreach (var city in response.Cities)
+        {
+            yield return new City(
+                NullCheck(city.Name),
+                NullCheck(city.CountryCode),
+                NullCheck(city.Admin1),
+                new Location(city.Lat, city.Lon),
+                city.Population
+            );
+        }
+    }
+
+    private class ApiResponse
+    {
+        [JsonPropertyName("results")]
+        public IEnumerable<ApiResponseCity> Cities { get; set; } = Enumerable.Empty<ApiResponseCity>();
+    }
+
+    [SuppressMessage("ReSharper", "ClassNeverInstantiated.Local")] // Instantiated by JSON deserializer
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")] // Used by JSON deserializer
+    private class ApiResponseCity
+    {
+        [JsonPropertyName("name")] public string? Name { get; set; }
+        [JsonPropertyName("country_code")] public string? CountryCode { get; set; }
+        [JsonPropertyName("admin1")] public string? Admin1 { get; set; }
+        [JsonPropertyName("latitude")] public float Lat { get; set; }
+        [JsonPropertyName("longitude")] public float Lon { get; set; }
+        [JsonPropertyName("population")] public int Population { get; set; }
+    }
+}
