7c9bb2f768b2a5139f839926cd98ddf474c506b9
KGergo02 <kassa.gergo2002@gmail.com>
2023-11-23 02:41:10 +0100

    Implemented first feature of the menu. Student data is now being pulled from the json file

--- HL347M_L123PZ/Commands/MenuCommand.cs
+++ HL347M_L123PZ/Commands/MenuCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudentTerminal.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +9,15 @@ namespace StudentTerminal.Commands
 {
     public static class MenuCommand
     {
-        public static void Menu()
+        public static async Task Menu()
         {
-            
+            List<Student> students = await StudentCommand.LoadStudentsFromJson();
 
             Console.Clear();
             Console.WriteLine("Válassz egy műveletet:");
-            Console.Write("1 - Felhasználók listázása\t");
-            Console.Write("2 - Felhasználók szűrése\t");
-            Console.Write("3 - Felhasználók rendezése\t");
+            Console.Write("1 - Tanulók listázása\t");
+            Console.Write("2 - Tanuló szűrése\t");
+            Console.Write("3 - Tanulók rendezése átlag alapján\t");
             Console.WriteLine("4 - Kilépés");
 
             int menu = Convert.ToInt32(Console.ReadLine());
@@ -26,27 +27,34 @@ namespace StudentTerminal.Commands
                 switch (menu)
                 {
                     case 1:
-                        Console.WriteLine("Első opció");
-                        Console.ReadLine();
-                        Menu();
+                        StudentCommand.Write(students);
+                        Console.WriteLine("A folytatáshoz nyomj meg egy gombot!");
+                        Console.ReadKey();
+                        await Menu();
                         return;
                     case 2:
                         Console.WriteLine("Második opció");
-                        Console.ReadLine();
-                        Menu();
+                        Console.WriteLine("A folytatáshoz nyomj meg egy gombot!");
+                        Console.ReadKey();
+                        await Menu();
                         return;
                     case 3:
                         Console.WriteLine("Harmadik opció");
-                        Console.ReadLine();
-                        Menu();
+                        Console.WriteLine("A folytatáshoz nyomj meg egy gombot!");
+                        Console.ReadKey();
+                        await Menu();
                         return;
                     default:
-                        Console.WriteLine("Nincs ilyen művelet!");
-                        Console.ReadLine();
-                        Menu();
+                        Console.WriteLine("Nincs ilyen művelet!\nA folytatáshoz nyomj meg egy gombot!");
+                        Console.ReadKey();
+                        await Menu();
                         return;
                 }
             }
+
+            Console.WriteLine("Köszönjük, hogy a StudentTerminál alkalmazást használta!");
+
+            Console.ReadKey();
         }
     }
 }
