f6cc46fff025d51f810f58d6681152c3f11d0d0b
opekope2 <opekope2@gmail.com>
2023-11-25 19:40:53 +0100

    Add simple analyzer (V1)

--- WeatherAnalyzer/Program.cs
+++ WeatherAnalyzer/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using WeatherAnalyzer.Cli;
 using WeatherAnalyzer.Geocode;
 using WeatherAnalyzer.Geocode.Api;
+using WeatherAnalyzer.Util;
 using WeatherAnalyzer.Weather;
 using WeatherAnalyzer.Weather.Api;
 
@@ -118,4 +119,94 @@ async Task<City?> ChooseCity(IAsyncEnumerable<City> cityOptions)
 
 async Task Analyze(IAsyncEnumerable<WeatherForecast> weatherForecasts)
 {
+    var allForecasts = await weatherForecasts.ToListAsync();
+
+    AnalyzeWeatherForecasts(allForecasts);
+    Console.WriteLine();
+
+    var byDay = from forecast in allForecasts
+        group forecast by DateOnly.FromDateTime(forecast.Time);
+
+    foreach (var day in byDay)
+    {
+        AnalyzeWeatherForecasts(day.ToList());
+        Console.WriteLine();
+    }
+
+    return;
+
+    void AnalyzeWeatherForecasts(IList<WeatherForecast> forecasts)
+    {
+        var times = (from forecast in forecasts
+            select DateOnly.FromDateTime(forecast.Time)).ToList();
+
+        var timeMin = times.Min();
+        var timeMax = times.Max();
+        Console.WriteLine(timeMin == timeMax ? $"{timeMin}:" : $"{timeMin}-{timeMax}:");
+
+        const char tab = '\t';
+
+        Console.Write(tab);
+        Console.WriteLine(CreateAnalysis(forecasts, "Temperature", f => f.Temperature));
+
+        Console.Write(tab);
+        Console.WriteLine(CreateAnalysis(forecasts, "Apparent temperature", f => f.ApparentTemperature));
+
+        var precipitationAnalysis = CreateAnalysis(forecasts, "Precipitation amount", f => f.Precipitation.Amount);
+        var precipitationProbabilityMin = forecasts.Min(f => f.Precipitation.Probability);
+        var precipitationProbabilityMax = forecasts.Max(f => f.Precipitation.Probability);
+        precipitationAnalysis =
+            $"{precipitationAnalysis}, probability: {precipitationProbabilityMin:P1}..{precipitationProbabilityMax:P1}";
+        Console.Write(tab);
+        Console.WriteLine(precipitationAnalysis);
+
+        var humidityMin = forecasts.Min(f => f.Humidity);
+        var humidityMax = forecasts.Max(f => f.Humidity);
+        var humidityAvg = forecasts.Average(f => f.Humidity);
+        Console.Write(tab);
+        Console.WriteLine($"Humidity: {humidityMin:P1}..{humidityMax:P1}, average: {humidityAvg:P1}");
+
+        Console.Write(tab);
+        Console.WriteLine(CreateAnalysis(forecasts, "Dew point", f => f.DewPoint));
+
+        Console.Write(tab);
+        Console.WriteLine(CreateAnalysis(forecasts, "Wind", f => f.Wind.Speed));
+    }
+
+    string CreateAnalysis(
+        IList<WeatherForecast> forecasts,
+        string by,
+        Func<WeatherForecast, ValueUnit<float>> selector
+    )
+    {
+        return CreateAnalysis2(forecasts, by, f => selector(f).Value, f => selector(f).Unit);
+    }
+
+    string CreateAnalysis2(
+        IList<WeatherForecast> forecasts,
+        string by,
+        Func<WeatherForecast, float> valueSelector,
+        Func<WeatherForecast, string> unitSelector
+    )
+    {
+        var (min, max, avg) = AnalyzeWeatherForecastsBy(forecasts, valueSelector);
+        var minValue = valueSelector(min);
+        var maxValue = valueSelector(max);
+        var unit = unitSelector(min);
+
+        return $"{by}: {minValue:F1}{unit}..{maxValue:F1}{unit}, average: {avg:F1}{unit}";
+    }
+
+    (WeatherForecast Min, WeatherForecast Max, float Average)
+        AnalyzeWeatherForecastsBy(IList<WeatherForecast> forecasts, Func<WeatherForecast, float> selector)
+    {
+        var min = forecasts.MinBy(selector);
+        var max = forecasts.MaxBy(selector);
+        if (min is null || max is null)
+        {
+            throw new ArgumentException("List was empty", nameof(forecasts));
+        }
+
+        return (min, max, forecasts.Average(selector));
+    }
 }
