96b0b939f13efabc6cb8346dddf34b31e3bd8fa9
ViDanMaster <nadalvokcs@gmail.com>
2023-12-03 14:35:23 +0100

    Made a JSON backup for all the tables

--- FTHEL8/Menu/MainMenu.cs
+++ FTHEL8/Menu/MainMenu.cs
@@ -1,4 +1,7 @@
-﻿namespace FTHEL8.Menu
+﻿using FTHEL8.Data;
+using Newtonsoft.Json;
+
+namespace FTHEL8.Menu
 {
     public class MainMenu : Menu
     {
@@ -15,6 +18,7 @@
             AddOption("Add Data", addMenu.Display);
             AddOption("Modify Data", modifyMenu.Display);
             AddOption("Delete Data", deleteMenu.Display);
+            AddOption("Make a backup save of the data", BackupToJson);
             AddOption("Exit", Exit);
         }
 
@@ -26,7 +30,34 @@
         private void Exit()
         {
             Console.WriteLine("Exiting the program.");
+
+            BackupToJson();
+
             Environment.Exit(0);
+        }
+
+        private void BackupToJson()
+        {
+            var employees = Database.ReadEmployeesAsync().Result;
+            var departments = Database.ReadDepartmentsAsync().Result;
+            var classes = Database.ReadClassesAsync().Result;
+            var projects = Database.ReadProjectsAsync().Result;
+            var projectMembers = Database.ReadProjectMembersAsync().Result;
+
+            var employeesBackupPath = "employees_backup.json";
+            var departmentsBackupPath = "departments_backup.json";
+            var classesBackupPath = "classes_backup.json";
+            var projectsBackupPath = "projects_backup.json";
+            var projectMembersBackupPath = "projectMembers_backup.json";
+
+
+            File.WriteAllText(employeesBackupPath, JsonConvert.SerializeObject(employees, Formatting.Indented));
+            File.WriteAllText(departmentsBackupPath, JsonConvert.SerializeObject(departments, Formatting.Indented));
+            File.WriteAllText(classesBackupPath, JsonConvert.SerializeObject(classes, Formatting.Indented));
+            File.WriteAllText(projectsBackupPath, JsonConvert.SerializeObject(projects, Formatting.Indented));
+            File.WriteAllText(projectMembersBackupPath, JsonConvert.SerializeObject(projectMembers, Formatting.Indented));
+
+            Console.WriteLine("Backup created successfully:");
         }
     }
 }
