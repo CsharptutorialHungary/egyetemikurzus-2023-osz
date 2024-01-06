03b55caee65f100bc8fe5d21fc42a17df96b4b87
kvcspt <kvcspt0@gmail.com>
2024-01-06 16:16:27 +0100

    English back

--- Q1EJTS/PersonalBudgetApp/Categories/FinancialCategory.cs
+++ Q1EJTS/PersonalBudgetApp/Categories/FinancialCategory.cs
@@ -1,17 +1,21 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Q1EJTS.PersonalBudgetApp.Categories
 {
     public enum FinancialCategory
     {
-        Bevetel,
-        Elelmiszer,
-        Lakber,
-        Szamlak,
-        Utazas,
-        Szorakozas,
-        Egeszseg,
-        Egyebkiadas
+        Income,
+        Food,
+        Rent,
+        Utilities,
+        Transportation,
+        Entertainment,
+        Health,
+        Other
     }
 
 }
